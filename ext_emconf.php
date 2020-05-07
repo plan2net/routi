a9c44ccd82344060328e0c3a6713fc913f1e85a9
Oliver Gassner <og@plan2.net>
2020-05-07 11:10:15 +0200

    [BUGFIX] Add missing site parameter for AspectFactory::createAspects

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Routi',
     'description' => 'The little TYPO3 CMS routing helpers',
-    'version' => '2.0.0',
+    'version' => '2.1.0',
     'state' => 'stable',
     'clearcacheonload' => true,
     'author' => 'Wolfgang Klinger',
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'plan2net GmbH',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.14-9.5.99'
+            'typo3' => '9.5.16-9.5.99'
         ]
     ]
 ];
